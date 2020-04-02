ccfd54a1b80e9ca79a6dea9b81621a6660661bf9
sudhakar3697 <sudax3@gmail.com>
2020-04-02 15:35:05 +0530

    Added get, add, remove gnome-favorite-apps functions

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # gnome-favorite-apps
 Get, Add, Remove Gnome favorite apps
+
+Require the module
+
+```
+const GnomeFavoriteApps = require('gnome-favorite-apps');
+```
+or
+
+```
+import GnomeFavoriteApps from 'gnome-favorite-apps';
+
+```
+
+Usage
+
+```
+(async () => {
+    console.log('Initially', await GnomeFavoriteApps.get());
+    await GnomeFavoriteApps.add('org.gnome.Nautilus.desktop', 'sqlitebrowser.desktop');
+    console.log('After adding', await GnomeFavoriteApps.get());
+    await GnomeFavoriteApps.remove('org.gnome.Nautilus.desktop', 'sqlitebrowser.desktop');
+    console.log('After removing', await GnomeFavoriteApps.get());
+})();
+
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+class GnomeFavoriteApps {
+    static async get() {
+        const favs = (await exec(`gsettings get org.gnome.shell favorite-apps`));
+        if (favs.stderr)
+            throw favs.stderr;
+        else
+            return favs.stdout.trim().replace(/\[/g, '').replace(/\]/g, '').replace(/\'/g, '').split(', ');
+    }
+
+    static async add(...apps) {
+        const appsToAdd = apps.map(app => `'${app}'`);
+        const favs = (await exec(`gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), ${appsToAdd.join(', ')}]"`));
+        if (favs.stderr)
+            throw favs.stderr;
+        else
+            return favs.stdout.trim();
+    }
+
+    static async remove(...apps) {
+        const appsToAdd = (await GnomeFavorites.get()).filter(app => !apps.includes(app)).map(app => `'${app}'`);
+        const favs = (await exec(`gsettings set org.gnome.shell favorite-apps "[${appsToAdd.join(', ')}]"`));
+        if (favs.stderr)
+            throw favs.stderr;
+        else
+            return favs.stdout.trim();
+    }
+}
+
+module.exports = GnomeFavoriteApps;
